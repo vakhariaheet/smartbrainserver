5d0b95d85c0e3934f13c05bba7ce647cdc3857f7
Heet Vakharia <aquisition@Heets-MacBook-Air.local>
2020-06-01 04:47:20 +0530

    msg

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const db = knex({
   });
   app.get('/db', async (req, res) => {
     try {
-      const client = await pool.connect();
+      const client = await db.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
